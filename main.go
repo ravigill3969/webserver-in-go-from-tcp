d328581b8a47bb9ac6d0c4032b345ce6447dda45
ravigill3936 <gill1234ravi@gmail.com>
2025-06-14 19:10:49 -0400

    using go channels to server files and read files and send it to client

--- main.go
+++ main.go
@@ -4,36 +4,42 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"os"
 )
+
+type Res struct {
+	message string
+	addr    string
+	file    string
+}
 
 func main() {
 	l, err := net.Listen("tcp", ":8080")
-
 	if err != nil {
-		fmt.Println("error in statrting server", err)
+		fmt.Println("Error in starting server:", err)
 		return
 	}
-
-	fmt.Println("server stated on port 8080")
-
+	fmt.Println("Server started on port 8080")
 	defer l.Close()
 
 	for {
 		c, err := l.Accept()
-
 		if err != nil {
-			fmt.Println("error accepting", err)
+			fmt.Println("Error accepting:", err)
 			continue
 		}
+		fmt.Println("Got a user connected")
 
-		fmt.Println("got a user connected ")
+		resCh := make(chan *Res)
 
-		go acceptConnection(c)
+		go acceptConnection(c, resCh)
+		go sendResponse(c, resCh)
+		go readHtmlFle(resCh, "index.html")
+
 	}
-
 }
 
-func acceptConnection(conn net.Conn) {
+func acceptConnection(conn net.Conn, resCh chan<- *Res) {
 	defer conn.Close()
 	buffer := make([]byte, 1024)
 
@@ -45,13 +51,53 @@ func acceptConnection(conn net.Conn) {
 			} else {
 				fmt.Println("Read error:", err)
 			}
+			close(resCh)
 			return
 		}
 
-		addr := conn.RemoteAddr()
+		message := string(buffer[:n])
+		addr := conn.RemoteAddr().String()
 
-		msg := fmt.Sprintf("Thanks for your message  %s\n", addr)
-		conn.Write([]byte(msg))
-		fmt.Printf("Received: %s\n", string(buffer[:n]))
+		fmt.Printf("Received: %s\n", message)
+
+		resCh <- &Res{
+			message: message,
+			addr:    addr,
+		}
+	}
+}
+
+func sendResponse(conn net.Conn, resCh <-chan *Res) {
+	defer conn.Close()
+	for res := range resCh {
+		if res.file != "" {
+			body := res.file
+			response := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
+				"Content-Type: text/html\r\n"+
+				"Content-Length: %d\r\n"+
+				"\r\n%s", len(body), body)
+
+			conn.Write([]byte(response))
+			return
+		} else {
+
+			msg := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
+				"Content-Type: text/plain\r\n"+
+				"Content-Length: %d\r\n"+
+				"\r\nThanks: %s", len(res.message)+8, res.message)
+
+			conn.Write([]byte(msg))
+		}
+	}
+}
+
+func readHtmlFle(resCh chan<- *Res, fileName string) {
+	f, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	resCh <- &Res{
+		file: string(f),
 	}
 }
